a3dd47fe4d83339193dcf24f9e8e2895297b40fd
alex chika <mcluckey1@gmail.com>
2023-03-16 02:53:49 +0100

    added fonts and restructured project

--- frontend/README.md
+++ frontend/README.md
@@ -1,3 +1,5 @@
 ### Development
 
-run `npm run dev` to start the postcss cli.
+This is a vanilla app powered with postcss and tailwind.
+
+To begin developememt run `npm run dev` to start the postcss cli which compiles the tailwind utility classes into `style.css` at the root of `frontend` dir.
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./html/**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      "switzer-reg": ['"Switzer-Regular"'],
+      "satoshi-black": ['"Satoshi-Black"'],
+      "switzer-bold": ['"Satoshi-Bold"'],
+    },
     extend: {},
   },
   plugins: [],
